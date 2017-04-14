469ac5fd336cdf1159014b7994aaee466d2a36e1
Jennifer Wang <jw775@cornell.edu>
2017-04-13 20:34:00 -0500

    add multiply function

--- client/multiply.js
+++ client/multiply.js
@@ -1,3 +1,9 @@
-const multiply = function(factor) {
-	//implement multiply action
+const multiply = function() {
+	if (arguments.length > 1) {
+		return Array.prototype.reduce.call(arguments, (result, factor) => result * factor);
+	} else {
+		return NaN;
+	}
 };
+
+module.exports = multiply;
--- client/test_multiply.js
+++ client/test_multiply.js
@@ -1,1 +1,19 @@
-//add test suite for multiply.js
+multiply = require('./multiply.js');
+
+function assertEquals(a, b) {
+	console.log(a === b ? "Passed!" : "Expected " + a + " but returned " + b + ".");
+};
+
+//test standard multiplication cases
+assertEquals(2, multiply(1,2));
+assertEquals(144, multiply(12,12));
+assertEquals(6, multiply(1,2,3));
+assertEquals(24, multiply(1,2,3,4));
+
+//test NaN scenarios
+assertEquals(true, isNaN(multiply(1)));
+assertEquals(true, isNaN(multiply()));
+
+//test negative factors
+assertEquals(-6, multiply(-1,2,3));
+assertEquals(6, multiply(-1,-2,3));
